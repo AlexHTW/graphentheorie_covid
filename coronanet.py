db49a1bdf34c6b72e8daa1ba2717d8987c225b06
jingyophuong <s0547007@htw-berlin.de>
2020-12-05 04:24:53 +0100

    implement number of cases -> size of node

--- coronanet.py
+++ coronanet.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -6,7 +7,9 @@ import math
 from datetime import datetime, date, timedelta
 import plotly.graph_objects as go
 from colour import Color
-
+from pandas import DataFrame
+import requests
+import json
 import random
 
 
@@ -41,6 +44,39 @@ R_VALUES = {
 
 R_VALUES['Countrywide'] = np.mean(list(R_VALUES.values()))
 
+
+def get_cases_data_json():
+	response = requests.get('https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/query?where=1%3D1&outFields=Bundesland,AnzahlFall,Meldedatum&outSR=800000000&f=json')
+	data_json = json.loads(response.text)['features']
+	
+	data = pd.json_normalize(data_json, sep='_')
+	data['attributes_Meldedatum'] = pd.to_datetime(data['attributes_Meldedatum'], unit='ms')
+	#print(len(data))
+	data = data.groupby(['attributes_Bundesland', 'attributes_Meldedatum'], as_index=False)['attributes_AnzahlFall'].sum()
+	
+	print(data.head())
+	data = data.sort_values(['attributes_Bundesland', 'attributes_Meldedatum'])
+	print(data)
+	#print(get_unique_vals(data, 'attributes_Bundesland'))
+	a =data[(data['attributes_Bundesland'] == 'Hamburg')]
+	#plt.show()
+	print(data.info())
+	print(len(a))
+
+def get_cases_data_csv():
+	url = "https://opendata.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0.csv"
+	data = pd.read_csv(url,
+					 encoding= 'utf8', 
+					 usecols=['Bundesland', 'AnzahlFall', 'Meldedatum'],
+					 parse_dates=['Meldedatum'])
+	
+	bundesland_cases = data.groupby(['Bundesland', 'Meldedatum'], as_index = False)['AnzahlFall'].sum()
+	bundesland_cases = bundesland_cases.set_index("Meldedatum").sort_index()
+	#TODO: r-values calculation
+	#bundesland_cases['r_value'] = bundesland_cases['AnzahlFall'].apply(lambda x: (bundesland_cases.iloc[-4:-1].sum(axis=1)) / (bundesland_cases.iloc[1:4].sum(axis=1)))
+	#print(bundesland_cases.head())
+	return bundesland_cases
+
 def get_data():
 	"""
 		gets 'live' data from github
@@ -48,6 +84,7 @@ def get_data():
 	country="Germany"
 	url="https://raw.githubusercontent.com/saudiwin/corona_tscs/master/data/CoronaNet/data_country/coronanet_release/coronanet_release_{0}.csv".format(country)
 	data=pd.read_csv(url, encoding='iso-8859-1')
+	#data["target_province"] = data["target_province"].str.decode('iso-8859-1').str.encode('utf-8')
 	return data
 
 def get_unique_vals(data, col = "type"):
@@ -67,7 +104,12 @@ def get_color_for_r_value(r_value):
 	new_max = N_COLORS - 1
 	idx = (((r_value - old_min) * (new_max - new_min)) / (old_max - old_min)) + new_min
 	return int(round(idx))
+def get_size_for_number_of_cases(number_of_cases):
+	"""
+		for given number_of_cases it returns size of node
+	"""
 
+	return (int(round(number_of_cases / 1000)) +1)*10
 def get_node_attr_by_key(nodes, key, attr):
 	"""
 		returns given attribute of element in nodes (=filtered by key)
@@ -107,14 +149,25 @@ def clean_bundeslaender(data):
 	# second Step: remove rows
 	# Data = All Data without having one of the values in brackets in the "taget_province" column
 	# "~" is like a not, so the filter afterwards is reversed
-	data = data[~(data.target_province.isin(["Berlin Brandenburg", "Bayern Baden-Württemberg", "Gütersloh, Warendorf"]))]
+	data = data[~(data.target_province.isin(["Berlin Brandenburg","Bayern Baden-Württemberg", "Gütersloh, Warendorf", "Lombardy"]))]
 
 	return data
 
+def clean_bundeslaender_cases(cases):
+	cases = cases.replace(to_replace=r'^Baden-Württemberg', value='Baden-Wuerttemberg', regex=True)
+	cases =cases.replace(to_replace=r'^Thüringen', value='Thuringia', regex=True)
+	cases = cases.replace(to_replace=r'^Bayern', value='Bavaria', regex=True)
+	cases = cases.replace(to_replace=r'^Niedersachsen', value='Lower Saxony', regex=True)
+	cases = cases.replace(to_replace=r'^Sachsen', value='Saxony', regex=True)
+	cases = cases.replace(to_replace=r'^Nordrhein-Westfalen', value='North Rhine-Westphalia', regex=True)
+	cases = cases.replace(to_replace=r'^Hessen', value='Hesse', regex=True)
+	return cases
 def create_graph():
+	#all data
 	data = get_data()
-	data = clean_bundeslaender(data)
-
+	data = clean_bundeslaender(data) #provinces and measure
+	
+	#get provinces and measure types
 	u_provinces = get_unique_vals(data, 'target_province')
 	u_types = get_unique_vals(data, 'type')
 
@@ -128,13 +181,20 @@ def create_graph():
 	threshold_2w = date.today() - timedelta(days=14)
 	threshold_4w = date.today() - timedelta(days=28)
 
-	#datasets
 	started_within_last_2w = data[data.date_start > threshold_2w]
 	ongoing = data[(data.date_start < threshold_2w) & (data.date_start > threshold_4w) & (data.date_end > threshold_2w)]
 	ongoing_4w = data[(data.date_start < threshold_4w) & (data.date_end > threshold_2w)]
 #	ended_within_2w = data[(data.date_start < threshold_4w & data.date_end > threshold_2w)] # TODO
 #	ended_2w_4w = data[(data.date_start < threshold_4w & data.date_end > threshold_2w)] # TODO
-
+	
+	#get current cases of all province
+	cases = get_cases_data_csv() #provinces and cases
+	yesterday = date.today() - timedelta(days=2) #only the number of infections up to yesterday was recorded
+	cases = cases.loc['{0}-{1}-{2}'.format(yesterday.year, yesterday.month, yesterday.day)]
+	cases = clean_bundeslaender_cases(cases)
+	cases = cases.reset_index().set_index("Bundesland")
+	cases.loc['Countrywide']= cases.sum(numeric_only=True, axis=0)
+	
 	##############
 	### PLOTLY ###
 	##############
@@ -145,6 +205,7 @@ def create_graph():
 		x = 0.35
 		y = i
 		r_value = R_VALUES[node]
+		num_of_infec = cases.loc[node]['AnzahlFall']
 		nodes.append({
 			'key': node,
 			'x': x,
@@ -152,7 +213,8 @@ def create_graph():
 			'textpos': "middle left",
 			'r_value': r_value,
 			'color': get_color_for_r_value(r_value),
-			'hovertext': 'R-Value: {0}'.format(r_value)
+			'hovertext': 'R-Value: {0} , Number of cases: {1}'.format(r_value, num_of_infec), 
+			'size': get_size_for_number_of_cases(num_of_infec)
 		})
 
 	for i,node in enumerate(u_types):
@@ -164,7 +226,8 @@ def create_graph():
 			'y': y,
 			'textpos': "middle right",
 			'color': "grey",
-			'hovertext': 'R-Value: {0}'.format(r_value)
+			'hovertext': 'R-Value: {0}'.format(r_value),
+			'size': 30
 		})
 
 	node_trace = go.Scatter(
@@ -183,7 +246,7 @@ def create_graph():
 			colorscale=COLORSCALE,
 			reversescale=True,
 			color = [node['color'] for node in nodes],
-			size=30,
+			size= [node['size'] for node in nodes],
 			colorbar=dict(
 				thickness=15,
 				title='R Value',
@@ -282,7 +345,7 @@ def create_graph():
                 hovermode='closest',
                 margin=dict(b=20,l=5,r=5,t=40),
                 annotations=[ dict(
-                    text="Data: <a href='https://www.coronanet-project.org/'> Coronanet Project</a>",
+                    text="Data: <a href='https://www.coronanet-project.org/'> Coronanet Project</a> | <a href= 'https://npgeo-corona-npgeo-de.hub.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0'> RKI Covid-19 </a>",
                     showarrow=False,
                     xref="paper", yref="paper",
                     x=0.005, y=-0.002 ) ],
@@ -293,3 +356,4 @@ def create_graph():
 
 if __name__ == '__main__':
 	create_graph()
+	
